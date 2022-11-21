f1057238c8e428b12245f1d25df171d79b739641
MirMoshref <117489824+MirMoshref@users.noreply.github.com>
2022-11-21 09:15:26 -0800

    Update GitPracticeClass.java
    This is from remote repo 2

--- src/GitPracticeClass.java
+++ src/GitPracticeClass.java
@@ -3,7 +3,7 @@ public class GitPracticeClass {
 
 	public static void main(String []args) {
 		
-		System.out.println("this is from local repo");
+		System.out.println("this is from REMOTE repo");
 		
 		System.out.println("this is new from local repo");
 		
